2d7ca22ebce4f6df5864b80e7f96b64a4a5efabf
sa <saekitakahiro@sanedakitakahiro-no-MacBook-Pro.local>
2016-02-25 15:29:37 +0900

    オブジェクト難しい

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,18 +1,24 @@
 const sample = { 1: '8.333%', 2: '16.666%', 3: '25%', 4: '33.333%', 5: '41.667%', 6: '50%', 7: '58.333%', 8: '66.667%', 9: '75%', 10: '83.333%', 11: '91.667%', 12: '100%' }
-class col {
-  constructor() {
 
-  }
-  default(colNum) {
-    let param =  {
+class col {
+  constructor(){
+    this.baseParam = {
       'box-sizing': 'border-box',
       'flex': '0 0 auto',
       'padding': '0 1rem',
-      'flex-basis': sample.colNum + '%',
-      'max-width': sample.colNum + '%'
     }
   }
+  default(colNum) {
+    let colParam1 = {
+      'flex-basis': sample[colNum],
+      'max-width': sample[colNum]
+    }
+    return Object.assign(this.baseParam, colParam1);
+  }
 }
+
+let colParam = new col();
+console.log(colParam.default(3))
 
 const baseCol = {
   'box-sizing': 'border-box',
@@ -43,150 +49,126 @@ new Descartes({
     'flex-direction': 'column-reverse'
   },
   '.col-xs': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'flex-grow': '1',
     'flex-basis': '0',
     'max-width': '100%'
   },
   '.col-xs-1': {
-    '_mixin': baseCol,
-    'flex-basis': '8.333%',
-    'max-width': '8.333%'
+    '_mixins': colParam.default(1)
   },
   '.col-xs-2': {
-    '_mixin': baseCol,
-    'flex-basis': '16.666%',
-    'max-width': '16.666%'
+    '_mixins': colParam.default(2)
   },
   '.col-xs-3': {
-    '_mixin': baseCol,
-    'flex-basis': '25%',
-    'max-width': '25%'
+    '_mixins': colParam.default(3)
   },
   '.col-xs-4': {
-    '_mixin': baseCol,
-    'flex-basis': '33.333%',
-    'max-width': '33.333%'
+    '_mixins': colParam.default(4)
   },
   '.col-xs-5': {
-    '_mixin': baseCol,
-    'flex-basis': '41.667%',
-    'max-width': '41.667%'
+    '_mixins': colParam.default(5)
   },
   '.col-xs-6': {
-    '_mixin': baseCol,
-    'flex-basis': '50%',
-    'max-width': '50%'
+    '_mixins': colParam.default(6)
   },
   '.col-xs-7': {
-    '_mixin': baseCol,
-    'flex-basis': '58.333%',
-    'max-width': '58.333%'
+    '_mixins': colParam.default(7)
   },
   '.col-xs-8': {
-    '_mixin': baseCol,
-    'flex-basis': '66.667%',
-    'max-width': '66.667%'
+    '_mixins': colParam.default(8)
   },
   '.col-xs-9': {
-    '_mixin': baseCol,
-    'flex-basis': '75%',
-    'max-width': '75%'
+    '_mixins': colParam.default(9)
   },
   '.col-xs-10': {
-    '_mixin': baseCol,
-    'flex-basis': '83.333%',
-    'max-width': '83.333%'
+    '_mixins': colParam.default(10)
   },
   '.col-xs-11': {
-    '_mixin': baseCol,
-    'flex-basis': '91.667%',
-    'max-width': '91.667%'
+    '_mixins': colParam.default(11)
   },
   '.col-xs-12': {
-    '_mixin': baseCol,
-    'flex-basis': '100%',
-    'max-width': '100%'
+    '_mixins': colParam.default(12)
   },
   '.col-xs-offset-0': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '0'
   },
   '.col-xs-offset-1': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '8.333%'
   },
   '.col-xs-offset-2': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '16.667%'
   },
   '.col-xs-offset-3': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '25%'
   },
   '.col-xs-offset-4': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '33.333%'
   },
   '.col-xs-offset-5': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '41.667%'
   },
   '.col-xs-offset-6': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '50%'
   },
   '.col-xs-offset-7': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '58.333%'
   },
   '.col-xs-offset-8': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '66.667%'
   },
   '.col-xs-offset-9': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '75%'
   },
   '.col-xs-offset-10': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '83.333%'
   },
   '.col-xs-offset-11': {
-    '_mixin': baseCol,
+    '_mixins': baseCol,
     'margin-left': '91.667%'
   },
-  '.start-xs' {
+  '.start-xs': {
     'justify-content': 'flex-start',
     'text-align': 'start'
   },
-  '.center-xs' {
-    'justify-content': 'center'
+  '.center-xs': {
+    'justify-content': 'center',
     'text-align': 'center'
   },
-  '.end-xs' {
-    'justify-content': 'flex-end'
+  '.end-xs': {
+    'justify-content': 'flex-end',
     'text-align': 'end'
   },
-  '.top-xs' {
-    'align-items': 'flex-start'
+  '.top-xs': {
+    'align-items': 'flex-start',
   },
-  '.middle-xs' {
-    'align-items': 'center'
+  '.middle-xs': {
+    'align-items': 'center',
   },
-  '.bottom-xs' {
+  '.bottom-xs': {
     'align-items': 'flex-end'
   },
-  '.around-xs' {
+  '.around-xs': {
     'justify-content': 'space-around'
   },
-  '.between-xs' {
+  '.between-xs': {
     'justify-content': 'space-between'
   },
-  '.first-xs' {
+  '.first-xs': {
     'order': '-1'
   },
-  '.last-xs' {
+  '.last-xs': {
     'order': '1'
   }
 })
